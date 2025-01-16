bb58efdbbf9741b6b16c1121cc59201058381ff0
Albert Hu <hualbert@rocketmail.com>
2025-01-16 01:53:53 -0800

    update

---
+++ src/data/lastest-projects.js
@@ -0,0 +1,43 @@
+const projects = [
+  {
+    img: "/assets/uploads/project2.jpg",
+    url: "/",
+    title: "Window Pond Project 1",
+  },
+  {
+    img: "/assets/uploads/project3.jpg",
+    url: "/",
+    title: "Window Pond Project 2",
+  },
+  {
+    img: "/assets/uploads/project4.jpg",
+    url: "/",
+    title: "Window Pond Project 3",
+  },
+  {
+    img: "/assets/uploads/project10.jpg",
+    url: "/",
+    title: "Window Pond Project 4",
+  },
+  {
+    img: "/assets/uploads/project6.jpg",
+    url: "/",
+    title: "Window Pond Project 5",
+  },
+  {
+    img: "/assets/uploads/project7.jpg",
+    url: "/",
+    title: "Window Pond Project 6",
+  },
+  {
+    img: "/assets/uploads/project8.jpg",
+    url: "/",
+    title: "Window Pond Project 7",
+  },
+  {
+    img: "/assets/uploads/project9.jpg",
+    url: "/",
+    title: "Window Pond Project 7",
+  },
+];
+export default projects;
